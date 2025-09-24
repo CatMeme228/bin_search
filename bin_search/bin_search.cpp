1ff3e69aebf43285fc38b175d8f4f36ba838b3ae
Анатолий Беляев <tolabelaev1251@gmail.com>
2025-09-24 19:05:57 +0300

    memory saved

--- bin_search/bin_search.cpp
+++ bin_search/bin_search.cpp
@@ -17,8 +17,8 @@ int bin_search(int left, int right, int x, int arr[]) {
 int main() {
 	int a[] = { 1,1,2,2,3,34,45,65,577,653,7809,1234567,765321};
 	int lenght = sizeof(a) / sizeof(a[0]);
-	int r = lenght - 1, num = 2;
-	printf("num index: %d", bin_search(0, r, num, a));
+	int num = 2;
+	printf("num index: %d", bin_search(0, lenght, num, a));
 
 	return 0;
 }
